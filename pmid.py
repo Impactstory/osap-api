3de0d671cf3deda80c915b928abc611ba9ceba8f
Heather Piwowar <hpiwowar@gmail.com>
2018-07-01 09:09:28 -0700

    add dataverse

--- pmid.py
+++ pmid.py
@@ -59,7 +59,7 @@ class Pmid(db.Model):
 
     def update_score_data(self):
         # self.score_data = 0
-        filter = "+and+(dryad+or+figshare)"
+        filter = "+and+(dryad+or+figshare+or+dataverse)"
         response = self.call_europepmc(filter)
         if response:
             print "found open data!"
