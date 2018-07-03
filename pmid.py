7febf09ae285bc2468a6180fd3a10a7c644380af
Heather Piwowar <hpiwowar@gmail.com>
2018-07-03 10:23:29 -0700

    add openneuro.org

--- pmid.py
+++ pmid.py
@@ -59,7 +59,7 @@ class Pmid(db.Model):
 
     def update_score_data(self):
         # self.score_data = 0
-        filter = "+and+(dryad+or+figshare+or+dataverse)"
+        filter = "+and+(dryad+or+figshare+or+dataverse+or+openneuro.org)"
         response = self.call_europepmc(filter)
         if response:
             print "found open data!"
